1ee060f1242038c599398e4c22759f72ecce3c01
Chris Marsh <chris.marsh@idems.international>
2023-11-17 14:45:53 +0000

    Initial commit

---
+++ config.ts
@@ -0,0 +1,36 @@
+import { generateDeploymentConfig } from "scripts";
+const config = generateDeploymentConfig("plh_facilitator_my");
+
+/**
+ * The default config should ideally be a superset of any extended configs
+ * to allow for easier post-processing
+ */
+
+config.google_drive = {
+  sheets_folder_ids: ["1k8zJfBeOm18gYDsk0XmdUm7OFtZDUY9R", "1rFg8qsmb3xSOXw_9Iwxk8RgSmyjqiao9"],
+  assets_folder_ids: ["1KcHDI7O4o2_FZ_YlXsz-8OqN3ehsfdVf", "1tr76W9OusHHkjJ4DNR84x-B26VgGCSFy"],
+};
+
+
+config.git = {
+  content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-mx-content.git",
+  content_tag_latest: "1.1.1",
+};
+
+config.api.db_name = "plh_facilitator_my"
+
+config.app_data.output_path = "./app_data";
+
+config.app_config.APP_LANGUAGES.default = "gb_en";
+config.app_config.APP_SIDEMENU_DEFAULTS.title = "Facilitator App";
+config.app_config.APP_HEADER_DEFAULTS.title = "Facilitator App";
+config.app_config.NOTIFICATION_DEFAULTS.title = "New message from Facilitator App";
+config.app_config.NOTIFICATION_DEFAULTS.text = "You have a new message from Facilitator App";
+config.app_config.APP_THEMES.available = ["default", "professional"];
+config.app_config.APP_THEMES.defaultThemeName = "professional";
+config.app_config.APP_UPDATES.enabled = true;
+config.app_config.APP_UPDATES.completeUpdateTemplate = "app_update_complete";
+config.app_config.APP_FOOTER_DEFAULTS.templateName = "footer";
+config.error_logging = { dsn: "https://12f94f1dda484e23bd027cb6dd518451@app.glitchtip.com/4993"};
+
+export default config;
